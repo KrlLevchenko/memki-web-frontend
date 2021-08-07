8c1923a800097f7db7894d5bc225c895e2f691c7
Kirill Levchenko <k.levchenko@dodopizza.com>
2021-08-07 11:41:25 +0300

    antd & prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Button } from 'antd'
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello world!</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+	<React.StrictMode>
+		<h1>Hello world!</h1>
+		<Button>Push me!</Button>
+	</React.StrictMode>,
+	document.getElementById('root'),
+)
